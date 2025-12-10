ee1eb16db7961bd4b24d2b006d85cbf829408a7b
azharBackendDev <azharakhtar189@gmail.com>
2025-12-10 18:10:39 +0530

    new update

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ import {router as authRouter} from './router/auth.router.js'
 import cors from "cors";
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: [process.env.FRONTEND_URL,"http://localhost:5173"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
